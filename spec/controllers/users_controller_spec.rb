52e3bd62b3355876246cab435ba8dda0d6cf01ed
Hina <hina@cs.umanitoba.ca>
2014-06-22 17:08:41 -0700

    Finish user signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
-describe UsersController do
+describe "User pages" do
 
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
+  subject { page }
+
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 
+  describe "signup page" do
+    before { visit signup_path }
+
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
 end
